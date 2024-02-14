d4fe4fb9b2245700992261c226c445a9488ade3d
Utkarsh Gupta <utkarsh.elia@gmail.com>
2024-02-14 23:04:54 +0530

    Student API using H2 embedded database

---
+++ src/main/java/com/springbootcrud/student/studentRepository/StudentRepository.java
@@ -0,0 +1,17 @@
+package com.springbootcrud.student.studentRepository;
+
+import com.springbootcrud.student.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findByStudentDepartmentAndStudentCity(String studentDepartment, String studentCity);
+
+    List<Student> findByStudentDepartment(String studentDepartment);
+
+    List<Student> findByStudentCity(String studentCity);
+}
